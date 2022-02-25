9be21754158e5a9ae846e8e8f03d5fb8de53200d
Govnor Payne <govnorpayne@gmail.com>
2022-02-25 09:00:33 -0600

    added reset funciton to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,29 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function (event) {
   event.preventDefault();
+
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  if (!taskNameInput || !taskTypeInput) {
+    alert("Input not valid! You need to fill out the task form");
+    return false;
+  }
+
+  formEl.reset();
+  document.querySelector("input[name='task-name']").value = "";
+  document.querySelector("select[name='task-type']").selectedIndex = 0;
 
   var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput,
   };
   createTaskEl(taskDataObj);
-  /*listItemEl.textContent = taskNameInput;*/
-  taskInfoEl.innerHTML =
-    "<h3 class='task-name'>" +
-    taskDataObj.name +
-    "</h3><span class='task-type'>" +
-    taskDataObj.type +
-    "</span>";
 };
 
 var createTaskEl = function (taskDataObj) {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
+
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
   taskInfoEl.innerHTML =
@@ -34,8 +37,10 @@ var createTaskEl = function (taskDataObj) {
     "</span>";
 
   listItemEl.appendChild(taskInfoEl);
+  console.dir(listItemEl);
+
   tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", taskFormHandler);
+//buttonEl.addEventListener("click", taskFormHandler);
 formEl.addEventListener("submit", taskFormHandler);
